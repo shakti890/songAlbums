2cd4ff88550dcd172210390d51590132caf7eaee
shakti890 <52260696+shakti890@users.noreply.github.com>
2020-05-20 20:03:18 +0530

    Add files via upload

---
+++ index.android.js
@@ -0,0 +1,16 @@
+//Import a liabrary to help create a component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+//Create a component
+const App = () => (
+    <View>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+    </View>
+
+);
+
+AppRegistry.registerComponent('albums', () => App);
